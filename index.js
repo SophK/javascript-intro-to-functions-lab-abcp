6f53dd5fb71a09063a6e7e68bc6d5061e2627c88
Sophie Kane <skane0207@gmail.com>
2018-01-16 19:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function logWhisper(string) {
 logWhisper('HELLO')
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string.toUpperCase() === string) {
     return ("YES INDEED!")
   }
@@ -30,3 +31,19 @@ function sayHiToGrandma(string) {
     }
   else { return ("I love you, too.")}
 }
+=======
+  var lowercase = string === "hello!"
+  var uppercase = string === "HELLO!"
+  var love = 'I love you, Grandma!'
+  if (love) {
+    return ("I love you, too.") }
+  else if (lowercase )  { 
+    return ("I can't hear you!") }
+  else if (uppercase) {
+    return ("YES INDEED!") }
+
+  }
+
+
+sayHiToGrandma ('hello!')
+>>>>>>> a7ad3fd0b995dc99b8055b3c6ec4308263bb9d06
